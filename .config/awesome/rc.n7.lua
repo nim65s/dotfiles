a218fd7befa1b16d4e57c7d23b989d91e49e0daf
nim65s.garbage@gmail.com <nim65s.garbage@gmail.com>
2011-05-18 19:52:50 +0200

    Session lock on Mod4 + "q"

--- .config/awesome/rc.n7.lua
+++ .config/awesome/rc.n7.lua
@@ -240,6 +240,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal)   end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
+    awful.key({ modkey,           }, "q",     function() awful.util.spawn("xscreensaver-command -lock") end),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
