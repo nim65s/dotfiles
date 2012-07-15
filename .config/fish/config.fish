f016feedf6b7551b0910c395d6e3ab329b96e7b5
Guilhem Saurel <guilhem.saurel@gmail.com>
2012-07-15 11:48:17 +0200

    Ajout de la première version du prompt de fish… Ne gère pas les VCS pour ne pas trop gacher les perfs, mais va falloir trouver une solution

---
+++ .config/fish/config.fish
@@ -0,0 +1,75 @@
+function fish_prompt
+    and set retc green; or set retc red
+
+    set_color $retc
+    echo -n ┬─
+    set_color -o green
+    echo -n [
+    if [ $USER = root ]
+        set_color -o red
+    else
+        set_color -o yellow
+    end
+    echo -n $USER
+    set_color -o white
+    echo -n @
+    if [ -z "$SSH_CLIENT" ]
+        set_color -o blue
+    else
+        set_color -o cyan
+    end
+    echo -n (hostname)
+    set_color -o white
+    #echo -n :(prompt_pwd)
+    echo -n :(pwd|sed "s=$HOME=~=")
+    set_color -o green
+    echo -n ']'
+    set_color normal
+    set_color $retc
+    echo -n ─
+    set_color -o green
+    echo -n '['
+    set_color normal
+    set_color $retc
+    echo -n (date +%X)
+    set_color -o green
+    echo -n ]
+    #set vcs none
+    #if [ -d .git ] # Là, j’ai honte…
+        #set vcs git
+    #else
+        #if [ -d .hg ]
+            #set vcs hg
+        #else
+            #if git symbolic-ref HEAD > /dev/null ^&1
+                #set vcs git
+            #else
+                #if hg root > /dev/null ^&1
+                    #set vcs hg
+                #end
+            #end
+        #end
+    #end
+    #echo -n $vcs
+    if [ (acpi -a 2> /dev/null | grep off) ]
+        echo -n '─['
+        set_color -o red
+        echo -n (acpi -b|cut -d' ' -f 4-)
+        set_color -o green
+        echo -n ']'
+    end
+    echo
+    set_color normal
+    for job in (jobs)
+        set_color $retc
+        echo -n '│ '
+        set_color brown
+        echo $job
+    end
+    set_color normal
+    set_color $retc
+    echo -n '╰─>'
+    set_color -o red
+    echo -n '$ '
+    set_color normal
+end
