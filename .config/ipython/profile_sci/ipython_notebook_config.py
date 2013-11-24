9d9e679b29207557305dff8cdc87d97ee482ffeb
Guilhem Saurel <guilhem.saurel@gmail.com>
2013-11-24 12:23:44 +0100

    conf i3

--- .config/ipython/profile_sci/ipython_notebook_config.py
+++ .config/ipython/profile_sci/ipython_notebook_config.py
@@ -5,7 +5,7 @@ c.IPKernelApp.pylab = 'inline'
 
 # Notebook config
 #c.NotebookApp.certfile = u'/absolute/path/to/your/certificate/mycert.pem'
-c.NotebookApp.ip = '*'
+#c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = True
 #c.NotebookApp.password = u'sha1:cf3fc87e49121b8ba30a92a1278dc89512681ef0'
 # It's a good idea to put it on a known, fixed port
