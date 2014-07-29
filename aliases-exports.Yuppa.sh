999dc4dfaf2e30dd8bab8b594e927d640de19150
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-07-29 05:02:23 +0200

    TERM set for mcabber…

--- aliases-exports.Yuppa.sh
+++ aliases-exports.Yuppa.sh
@@ -1,1 +1,2 @@
-alias mcabber='tmux has-session -t mcabber && tmux attach -d -t mcabber || tmux new -s mcabber -n client mcabber'
+alias mcabber='tmux has-session -t mcabber && tmux attach -d -t mcabber || tmux new -s mcabber -n client "TERM=rxvt-256color mcabber"'
+
