24341c41a1c1e170898d4e35de79f8847df57529
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-07-29 14:49:31 +0900

    suppression du profile_sci

--- .config/ipython/profile_sci/startup/00-imports.py
+++
@@ -1,7 +0,0 @@
-from IPython.display import Image
-from sympy.physics.unitsystems.mask import *
-from sympy.physics.unitsystems.units import *
-from math import pi
-import numpy
-import pylab
-set_system(mask)
