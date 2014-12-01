e997037fdf6f89339160b4c7ce5052ba78ddd55a
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-12-01 16:13:45 +0100

    virtualfish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
+set -g VIRTUALFISH_COMPAT_ALIASES
+. virtualfish/virtual.fish
+. virtualfish/auto_activation.fish
+. virtualfish/global_requirements.fish
+. virtualfish/projects.fish
+
 if status --is-login
     if not set -q LANG >/dev/null
         set -gx LANG fr_FR.UTF-8
@@ -69,6 +75,20 @@ function fish_prompt
    #    echo -n ']'
    #end
     set_color normal
+    if set -q VIRTUAL_ENV
+        set_color $retc
+        if [ $tty = tty ]
+            echo -n '-'
+        else
+            echo -n '─'
+        end
+        set_color -o green
+        echo -n '['
+        set_color normal
+        echo -n (basename "$VIRTUAL_ENV")
+        set_color -o green
+        echo -n ']'
+    end
     echo -n (__fish_git_prompt)
     echo
     set_color normal
@@ -184,6 +204,11 @@ set -x EDITOR vim
 set -x BROWSER chromium
 set -x JAVA_HOME /opt/java
 set -x PAGER ~/scripts/vimpager/vimpager
+
+set -x PIP_USE_WHEEL "true"
+set -x PIP_WHEEL_DIR "$HOME/.pip/wheels"
+set -x PIP_FIND_LINKS "$HOME/.pip/wheels"
+set -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
 
 set -x LS_COLORS 'rs=0:di=01;33:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lz=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.axa=00;36:*.oga=00;36:*.spx=00;36:*.xspf=00;36:'
 
