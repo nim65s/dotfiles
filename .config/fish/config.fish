4a5b2b82a842051168f6d918b50455d6d0de6acc
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-06-26 19:17:30 +0200

    __fish_git_prompt si fish version == 2

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -77,7 +77,8 @@ function fish_prompt
     and nim_prompt_wrapper $retc $tty V basename "$VIRTUAL_ENV"
     acpi -a 2> /dev/null | grep -q off
     and nim_prompt_wrapper $retc $tty B 'acpi -b | cut -d: -f2- | sed ":a;N;\$!ba;s/\n/|/g"'
-    echo -n (__fish_git_prompt)
+    test (fish -v 2>|cut -d' ' -f3| cut -d'.' -f1) -eq 2
+    and echo -n (__fish_git_prompt)
     echo
     set_color normal
     for job in (jobs)
