e8b8f8de4319ec7b784b6a77ec507accd50cd3fe
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-12-19 21:59:28 +0100

    posix-source

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -200,6 +200,14 @@ function srihash
     end
 end
 
+# thx http://lewandowski.io/2016/10/fish-env/
+function posix-source -d "loads a POSIX environment file"
+    for i in (cat $argv)
+        set arr (echo $i | tr = \n)
+        set -gx $arr[1] $arr[2]
+    end
+end
+
 . ~/dotfiles/portable-aliases.sh
 . ~/dotfiles/.config/fish/completions
 
