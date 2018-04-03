f5dced91478ca1ca6adcc4123528c7b5027ee176
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-04-03 10:22:35 +0200

    akasan

---
+++ .config/fish/akasan.fish
@@ -0,0 +1,19 @@
+function ii
+    cd
+    while true
+        pass test
+        and vdirsyncer sync
+        and rsync -avP --delete .vdir root@jiro.saurel.me:
+        and echo fini
+        or echo fail
+        sleep 300
+    end
+end
+
+function matin
+    snapclient -h hagurosan > /dev/null &
+    khal
+    ii
+end
+
+set -x SSH_AUTH_SOCK /home/gsaurel/.gnupg/S.gpg-agent.ssh
