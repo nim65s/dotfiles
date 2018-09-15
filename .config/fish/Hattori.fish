ca92f32b6d02166c879ef95da1923f3595180918
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-09-15 18:23:14 +0200

    hattori: ROBOTPKG_BASE

--- .config/fish/Hattori.fish
+++ .config/fish/Hattori.fish
@@ -3,3 +3,6 @@ set -x MINIKUBE_WANTREPORTERRORPROMPT false
 set -x MINIKUBE_HOME $HOME
 set -x CHANGE_MINIKUBE_NONE_USER true
 set -x KUBECONFIG $HOME/.kube/config
+
+set -x ROBOTPKG_BASE /opt/openrobots
+set -x PKG_CONFIG_PATH $ROBOTPKG_BASE/lib/pkgconfig/
