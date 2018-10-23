2c1eeea29cb275108891868418dc02d1909ea51a
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-10-23 11:29:29 +0200

    hattori: ROBOTPKG_BASE

--- .config/fish/Hattori.fish
+++ .config/fish/Hattori.fish
@@ -1,8 +1,2 @@
-set -x MINIKUBE_WANTUPDATENOTIFICATION false
-set -x MINIKUBE_WANTREPORTERRORPROMPT false
-set -x MINIKUBE_HOME $HOME
-set -x CHANGE_MINIKUBE_NONE_USER true
-set -x KUBECONFIG $HOME/.kube/config
-
-set -x ROBOTPKG_BASE /opt/openrobots
+set -x ROBOTPKG_BASE /home/nim/local/robotpkg/install
 set -x PKG_CONFIG_PATH $ROBOTPKG_BASE/lib/pkgconfig/
