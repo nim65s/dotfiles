b959344f549120bd8b26098bf6982ceb18df6e14
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-10-26 08:13:31 +0200

    nausicaa: robotpkg

--- .config/fish/Nausicaa.fish
+++ .config/fish/Nausicaa.fish
@@ -4,5 +4,8 @@ set -x DEVEL_PREFIX ~/local/prefix
 set -x LD_LIBRARY_PATH $DEVEL_PREFIX/lib
 set -x PKG_CONFIG_PATH $DEVEL_PREFIX/lib/pkgconfig
 
-set -x ROBOTPKG_BASE /opt/openrobots
+set -x ROBOTPKG_BASE ~/local/robotpkg/install
 set -x PKG_CONFIG_PATH $ROBOTPKG_BASE/lib/pkgconfig:$PKG_CONFIG_PATH
+set -x CMAKE_PREFIX_PATH $ROBOTPKG_BASE
+test -d $ROBOTPKG_BASE/bin
+and set -x PATH $ROBOTPKG_BASE/bin $PATH
