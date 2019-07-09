f2e60b5c8a668444105c27fb60031f74904e1347
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-07-09 22:18:07 +0200

    LD_LIBRARY_PATH

--- .config/fish/Hattori.fish
+++ .config/fish/Hattori.fish
@@ -1,3 +1,4 @@
 set -x ROBOTPKG_BASE /home/nim/local/robotpkg/install
 set -x PKG_CONFIG_PATH $ROBOTPKG_BASE/lib/pkgconfig/
 set -x PATH $ROBOTPKG_BASE/bin $PATH
+set -x LD_LIBRARY_PATH $ROBOTPKG_BASE/lib
