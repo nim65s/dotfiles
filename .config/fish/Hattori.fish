0cedc826a9c33880090b73d4f505e421c723ab91
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-08-06 22:45:21 +0200

    ls -F

--- .config/fish/Hattori.fish
+++ .config/fish/Hattori.fish
@@ -2,3 +2,4 @@ set -x ROBOTPKG_BASE /home/nim/local/robotpkg/install
 set -x PKG_CONFIG_PATH $ROBOTPKG_BASE/lib/pkgconfig/
 set -x PATH $ROBOTPKG_BASE/bin $PATH
 set -x LD_LIBRARY_PATH $ROBOTPKG_BASE/lib
+set -x ROS_PACKAGE_PATH $ROBOTPKG_BASE/share
