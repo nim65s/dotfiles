adbeb6bc159ba8fb4d5f246699d2c2a7a18bbf3a
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-01-06 13:06:39 +0100

    fish: auto poetry

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -229,6 +229,7 @@ if command -s pipenv > /dev/null
             return
         end
         if not test -e "$PWD/Pipfile"
+
             if not string match -q "$__pipenv_fish_initial_pwd"/'*' "$PWD/"
                 set -U __pipenv_fish_final_pwd "$PWD"
                 exit
@@ -255,12 +256,41 @@ else
     end
 end
 
+# https://github.com/fisherman/pipenv/blob/master/conf.d/pipenv.fish %s/pipenv/poetry
+if command -s poetry > /dev/null
+    function __poetry_shell_activate --on-variable PWD
+        if status --is-command-substitution
+            return
+        end
+       if not test -e "$PWD/pyproject.toml"
+            if not string match -q "$__poetry_fish_initial_pwd"/'*' "$PWD/"
+                set -U __poetry_fish_final_pwd "$PWD"
+                exit
+            end
+            return
+        end
+        if not test -n "$POETRY_ACTIVE"
+          if test (poetry env list | wc -l) -gt 0
+            set -x __poetry_fish_initial_pwd "$PWD"
+            poetry shell -q
+            set -e __poetry_fish_initial_pwd
+            if test -n "$__poetry_fish_final_pwd"
+                cd "$__poetry_fish_final_pwd"
+                set -e __poetry_fish_final_pwd
+            end
+          end
+        end
+    end
+end
+
+
 function fish_greeting
     #command -qs ponysay
     #and ponysay -o
 end
 
 function ``` --description 'no-op, to ease copy-paste from markdown'
+    # ```
     return 0
 end
 
