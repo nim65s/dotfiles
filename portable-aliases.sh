08dc6a5d46e29bc8bee521907aa8e4095a325d39
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-03-30 12:24:11 +0200

    update nausicaa output layout

--- portable-aliases.sh
+++ portable-aliases.sh
@@ -126,3 +126,4 @@ alias refl="sudo reflector --verbose --latest 100 --sort rate --save /etc/pacman
 alias catkin_make="catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python2 -DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so"
 
 alias im='mosh -- mononoke tmux a -d -t im'
+alias civ='env LD_PRELOAD=/usr/lib32/libopenal.so.1 steam steam://rungameid/8930'
