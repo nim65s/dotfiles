fbb007f4c25ee2ccc955a9dd0fb1e22de7be1f04
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-03 10:45:06 +0200

    idf_path

--- .config/fish/Nausicaa.fish
+++ .config/fish/Nausicaa.fish
@@ -2,6 +2,8 @@
 #set -x LD_LIBRARY_PATH $DEVEL_PREFIX/lib
 #set -x PKG_CONFIG_PATH $DEVEL_PREFIX/lib/pkgconfig
 
+set -e IDF_PATH
+
 set -x ROBOTPKG_BASE ~/local/robotpkg/install
 set -x PKG_CONFIG_PATH $ROBOTPKG_BASE/lib/pkgconfig:$ROBOTPKG_BASE/share/pkgconfig:$PKG_CONFIG_PATH
 set -x CMAKE_PREFIX_PATH $ROBOTPKG_BASE
@@ -18,5 +20,8 @@ and set -x PKG_DBDIR $ROBOTPKG_BASE/var/db/robotpkg
 
 test -d /home/nim/celeste/ESP/esp-idf
 and set -x IDF_PATH /home/nim/celeste/ESP/esp-idf
+
+test -d /home/nim/misc/ESP/esp-idf
+and set -x IDF_PATH /home/nim/misc/ESP/esp-idf
 
 set -x DOCKER_BUILDKIT 1
