f90e7a292a53ef8688fbfda267218728d9079cd8
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-09-13 09:11:15 +0200

    cvi

--- portable-aliases.sh
+++ portable-aliases.sh
@@ -131,7 +131,8 @@ alias refl="sudo reflector --verbose --latest 100 --sort rate --save /etc/pacman
 alias catkin_make="catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python2 -DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so"
 
 alias im='mosh -- mononoke tmux a -d -t im'
-alias civ='env LD_PRELOAD=/usr/lib32/libopenal.so.1 steam steam://rungameid/8930'
+alias civ5='env LD_PRELOAD=/usr/lib32/libopenal.so.1 steam steam://rungameid/8930'
+alias civ6='env LD_PRELOAD=/usr/lib/libfreetype.so.6 steam steam://rungameid/289070'
 
 #CMake
 alias bn='cmake -B build -G Ninja'
