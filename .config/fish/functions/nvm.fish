8c3f6686e4dc671ec88ce9707a01d4fde8bedea8
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-11-07 18:02:18 +0100

    misc

--- .config/fish/functions/nvm.fish
+++ .config/fish/functions/nvm.fish
@@ -1,37 +1,32 @@
-function nvm-fast
-	set -l brigand_nvm_fish_path ~/.nvm/versions/node
-	if test (count $argv[1]) -lt 1
-		echo 'nvm-fast: at least one argument is required'
-	end
-
-	set -l command $argv[1]
-	if test $command = 'use'
-		set -l target_version $argv[2]
-		set -l matched_version (bash -c "source ~/.nvm/nvm.sh --no-use; nvm_version $target_version")
-		if test -z $matched_version -o $matched_version = 'N/A'
-			echo "No version installed for $target_version, run nvm install $target_version"
-			echo "Installed versions: "
-			for file in $brigand_nvm_fish_path/v*
-				echo ' -' $file
-			end
-		else
-			set -l new_path
-			for path_segment in $fish_user_paths
-				if not echo "$path_segment" | grep -q "$brigand_nvm_fish_path"
-					set new_path $new_path "$path_segment"
-				end
-			end
-			if test $matched_version != 'system'
-				set new_path $brigand_nvm_fish_path/$matched_version/bin $new_path
-			end
-			set -g fish_user_paths $new_path
-		end
-	else
-		bash -c "source ~/.nvm/nvm.sh --no-use; nvm $argv"
-	end
-end
+test -z "$NVM_DIR"; and set -x NVM_DIR "$HOME/.nvm"
 
 function nvm
-	nvm-fast $argv
+  bass source "$NVM_DIR/nvm.sh" --no-use ';' nvm $argv
 end
 
+# ~/.config/fish/functions/nvm_find_nvmrc.fish
+function nvm_find_nvmrc
+  bass source "$NVM_DIR/nvm.sh" --no-use ';' nvm_find_nvmrc
+end
+
+# ~/.config/fish/functions/load_nvm.fish
+function load_nvm --on-variable="PWD"
+  set -l default_node_version (nvm version default)
+  set -l node_version (nvm version)
+  set -l nvmrc_path (nvm_find_nvmrc)
+  if test -n "$nvmrc_path"
+    set -l nvmrc_node_version (nvm version (cat $nvmrc_path))
+    if test "$nvmrc_node_version" = "N/A"
+      nvm install (cat $nvmrc_path)
+    else if test "$nvmrc_node_version" != "$node_version"
+      nvm use $nvmrc_node_version
+    end
+  else if test "$node_version" != "$default_node_version"
+    echo "Reverting to default Node version"
+    nvm use default
+  end
+end
+
+# ~/.config/fish/config.fish
+# You must call it on initialization or listening to directory switching won't work
+#load_nvm > /dev/stderr
