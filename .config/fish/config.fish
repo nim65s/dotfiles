bbd5247fe06ec3161b56a49dc2e1e10767dfbb62
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-05-11 08:40:19 +0200

    sass-cache

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -129,7 +129,7 @@ function ii
         and offlineimap
         and ~/scripts/nmnnn.sh
         and vdirsyncer sync
-        and ~/scripts/rtt.py -np ~/Nextcloud/rtt.json
+        #and ~/scripts/rtt.py -np ~/Nextcloud/rtt.json
         and echo fini
         or echo fail
         date
