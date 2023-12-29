fff6fe08cef118cf2ab977420d2baf9deceaa884
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-12-29 18:34:16 +0100

    cp yupa.nix hattorixos.nix

---
+++ .config/home-manager/hattorixos.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, lib, ... }:
+
+let
+  username = "nim";
+in {
+  imports = [ ./common.nix ];
+
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
+  programs.waybar.settings.mainBar.output = "eDP-1";
+  xsession.windowManager.i3.config = import ./i3swayconfig.nix { lib=lib; sway=false; pkgs=pkgs; };
+  wayland.windowManager.sway.config = import ./i3swayconfig.nix { lib=lib; sway=true; pkgs=pkgs; } // {
+    output = { "*" = { bg = "${./bg/yupa.jpg} fill"; }; };
+  };
+  wayland.windowManager.hyprland.settings = {
+    exec-once = [
+      "hyprpaper"
+      "waybar"
+      "firefox"
+      "thunderbird"
+      "element-desktop"
+    ];
+    monitor = [
+      "eDP-1, 1920x1080, 0x0, 1"
+    ];
+    workspace = [
+      "1, default:true"
+      "2"
+      "3"
+      "4"
+      "5"
+      "6"
+      "7"
+      "8"
+      "9"
+      "10, gapsin:0, gapsout:0, rounding:0, decorate:0, default:true"
+      "11, gapsin:0, gapsout:0, rounding:0, decorate:0"
+      "12, gapsin:0, gapsout:0, rounding:0, decorate:0"
+    ];
+  };
+}
