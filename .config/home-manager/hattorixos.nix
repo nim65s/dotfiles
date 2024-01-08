4551a8f23064e89aeddff3a21f6b375bb5a3e36f
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-01-08 12:17:33 +0100

    hm: update sway for yupa & hattorioxs

--- .config/home-manager/hattorixos.nix
+++ .config/home-manager/hattorixos.nix
@@ -2,15 +2,30 @@
 
 let
   username = "nim";
+  workspaceOutputAssign = [];
 in {
   imports = [ ./common.nix ];
 
   home.username = username;
   home.homeDirectory = "/home/${username}";
   programs.waybar.settings.mainBar.output = "eDP-1";
-  xsession.windowManager.i3.config = import ./i3swayconfig.nix { lib=lib; sway=false; pkgs=pkgs; };
-  wayland.windowManager.sway.config = import ./i3swayconfig.nix { lib=lib; sway=true; pkgs=pkgs; } // {
-    output = { "*" = { bg = "${./bg/sleep.jpg} fill"; }; };
+  xsession.windowManager.i3.config = import ./i3swayconfig.nix {
+    lib=lib;
+    sway=false;
+    pkgs=pkgs;
+    workspaceOutputAssign=workspaceOutputAssign;
+  };
+  wayland.windowManager.sway.config = import ./i3swayconfig.nix {
+    lib=lib;
+    sway=true;
+    pkgs=pkgs;
+    workspaceOutputAssign=workspaceOutputAssign;
+  } // {
+    output = {
+      "*" = {
+        bg = "${./bg/sleep.jpg} fill";
+      };
+    };
   };
   wayland.windowManager.hyprland.settings = {
     exec-once = [
