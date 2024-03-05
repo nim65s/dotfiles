1d6c6718204e60485630ff75546b28393e37e8a4
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-05 13:14:06 +0100

    hattorixos: fix unfree

--- nixos/hattorixos/configuration.nix
+++ nixos/hattorixos/configuration.nix
@@ -81,9 +81,6 @@
     packages = [ ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
