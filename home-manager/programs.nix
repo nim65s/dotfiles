ffd5e20a312515966c904dbbfc3e0095f0462c7a
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-15 13:37:33 +0100

    drop mySway

--- home-manager/programs.nix
+++ home-manager/programs.nix
@@ -12,33 +12,6 @@ let
       host ? "laas.fr",
     }:
     "${name}@${host}";
-  mySway = pkgs.sway.override {
-    sway-unwrapped =
-      (pkgs.sway-unwrapped.overrideAttrs (
-        finalAttrs: previousAttrs: {
-          patches = lib.lists.take 2 previousAttrs.patches ++ lib.lists.drop 3 previousAttrs.patches;
-          src = pkgs.fetchFromGitHub {
-            owner = "nim65s";
-            repo = "sway";
-            rev = "fa4c1cdc50b1cf28acac4e599b750a65e788602e";
-            hash = "sha256-NbmjZKuu1c+m293Vzi35EEjBEWaOfp0F0pz7rtKesJU=";
-          };
-        }
-      )).override
-        {
-          wlroots_0_16 = pkgs.wlroots.overrideAttrs {
-            version = "0.18.0-dev";
-            src = pkgs.fetchFromGitLab {
-              domain = "gitlab.freedesktop.org";
-              owner = "wlroots";
-              repo = "wlroots";
-              rev = "48721bca656556606275a5e776066a2f00822e92";
-              hash = "sha256-PUx4RZiLbWineoAvZk7kuUBXRFI15vfxLna49LUR8+s=";
-            };
-            patches = [ ];
-          };
-        };
-  };
 in
 {
   programs = {
@@ -100,7 +73,7 @@ in
         and source ~/dotfiles/.config/fish/path.fish
 
         if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-          ${config.nixGL} ${lib.getExe mySway} > ~/.wayland.log 2> ~/.wayland.err
+          ${config.nixGL} ${lib.getExe pkgs.sway} > ~/.wayland.log 2> ~/.wayland.err
         end
       '';
       shellAliases = {
