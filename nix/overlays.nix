1a24d72163347ce09da765dbadbe49abe01a9212
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-19 12:59:11 +0100

    sway: master neds wlroots 0.18

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,16 +1,30 @@
 self: super:
 let
   lib = self.lib;
- sway-unwrapped = super.sway-unwrapped.overrideAttrs (
-    previous: {
-      src = self.fetchFromGitHub {
-        owner = "svalaskevicius";
-        repo = "sway";
-        rev = "hiding-lone-titlebar-scenegraph";
-        hash = "sha256-cXBEXWUj3n9txzpzDgl6lsNot1ag1sEE07WAwfCLWHc=";
+  sway-unwrapped =
+    (super.sway-unwrapped.overrideAttrs (
+      finalAttrs: previousAttrs: {
+        src = self.fetchFromGitHub {
+          owner = "svalaskevicius";
+          repo = "sway";
+          rev = "hiding-lone-titlebar-scenegraph";
+          hash = "sha256-cXBEXWUj3n9txzpzDgl6lsNot1ag1sEE07WAwfCLWHc=";
+        };
+      }
+    )).override
+      {
+        wlroots = super.wlroots.overrideAttrs {
+          version = "0.18.0-dev";
+          src = self.fetchFromGitLab {
+            domain = "gitlab.freedesktop.org";
+            owner = "wlroots";
+            repo = "wlroots";
+            rev = "48721bca656556606275a5e776066a2f00822e92";
+            hash = "sha256-PUx4RZiLbWineoAvZk7kuUBXRFI15vfxLna49LUR8+s=";
+          };
+          patches = [ ];
+        };
       };
-    }
-  );
 in
 {
   gruppled-white-lite-cursors = self.callPackage ./gruppled-lite-cursors {
