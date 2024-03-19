5b73c08bfc022d57b0c1b1ff120e674a86272d1a
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-19 12:15:12 +0100

    loon: amdgpu

--- nix/loon/hardware-configuration.nix
+++ nix/loon/hardware-configuration.nix
@@ -19,7 +19,7 @@
     "usb_storage"
     "sd_mod"
   ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
