5de732c891df805a88b0631b6130321bf093bf39
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-04-28 03:31:11 +0200

    fix: enable fish

--- nix/fix/configuration.nix
+++ nix/fix/configuration.nix
@@ -180,4 +180,6 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
+
+  programs.fish.enable = true;
 }
