fcfde4c71b3b42a57953a754307a21fe2c42bf06
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-04-28 02:56:24 +0200

    git fix initial cfg

--- nix/fix/configuration.nix
+++ nix/fix/configuration.nix
@@ -2,7 +2,8 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ... }:
+
 {
   imports = [
     # Include the results of the hardware scan.
@@ -13,12 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Setup keyfile
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
-
-  networking.hostName = "loon"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -46,61 +42,87 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   # Configure keymap in X11
-  #xserver.xkb = {
-  #  layout = "fr";
-  #  variant = "bepo";
-  #};
+  services.xserver = {
+    layout = "fr";
+    xkbVariant = "";
+  };
 
   # Configure console keymap
-  console.keyMap = "fr-bepo";
+  console.keyMap = "fr";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nim = {
-    shell = pkgs.fish;
-    isNormalUser = true;
-    description = "Guilhem Saurel";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "docker"
-      "video"
-    ];
-    packages = with pkgs; [ ];
+  users.users = {
+    fil = {
+      isNormalUser = true;
+      description = "Philippe Saurel";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+      ];
+      packages = with pkgs; [
+        firefox
+        #  thunderbird
+      ];
+    };
+    nim = {
+      isNormalUser = true;
+      description = "Guilhem Saurel";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+      ];
+      packages = [ ];
+    };
   };
 
   # Enable automatic login for the user.
-  services.getty.autologinUser = "nim";
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "fil";
 
-  nix = {
-    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-    settings = {
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
-        "https://nim65s-dotfiles.cachix.org"
-        "https://nim65s-nur.cachix.org"
-        "https://rycee.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nim65s-dotfiles.cachix.org-1:6vuY5z8YGzfjrssfcxb3DuH50DC1l562U0BIGMxnClg="
-        "nim65s-nur.cachix.org-1:V3uaUnDnkWYgPDZaXpoe/KIbX5913GWfkazhHVDYPoU="
-        "rycee.cachix.org-1:TiiXyeSk0iRlzlys4c7HiXLkP3idRf20oQ/roEUAh/A="
-      ];
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-    };
-  };
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
+    #  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -120,7 +142,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = true;
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -129,29 +151,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
-  fonts = {
-    fontconfig = {
-      defaultFonts = {
-        serif = [ "Source Serif 4" ];
-        sansSerif = [ "Source Sans 3" ];
-        monospace = [ "SauceCodePro Nerd Font" ];
-      };
-    };
-  };
-  hardware.opengl.enable = true;
-  programs.dconf.enable = true;
-  programs.fish.enable = true;
-  virtualisation.docker.enable = true;
-  sound.enable = false;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    audio.enable = true;
-    pulse.enable = true;
-  };
-  #xdg.portal.wlr.enable = true;
 }
--- nix/fix/hardware-configuration.nix
+++ nix/fix/hardware-configuration.nix
@@ -13,37 +13,37 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd.availableKernelModules = [
-    "nvme"
     "xhci_pci"
     "thunderbolt"
+    "nvme"
     "usb_storage"
     "sd_mod"
   ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/3b47c651-c55f-4384-8c84-ea8fd535c7d2";
+    device = "/dev/disk/by-uuid/34fd5c9a-a2c9-4861-b696-b1bfb14ec9d4";
     fsType = "ext4";
   };
 
-  boot.initrd.luks.devices."luks-79f51bc8-4cb7-4dde-820b-1e4403cb8a25".device = "/dev/disk/by-uuid/79f51bc8-4cb7-4dde-820b-1e4403cb8a25";
-
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/2EFE-CDA0";
+    device = "/dev/disk/by-uuid/D0ED-A303";
     fsType = "vfat";
   };
 
-  swapDevices = [ { device = "/dev/disk/by-uuid/eafbd18d-fb85-4978-84e0-a22a95027cdf"; } ];
+  swapDevices = [ { device = "/dev/disk/by-uuid/c29c5b43-35d1-4a41-82db-3a3640a927d5"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s13f0u2c2.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp170s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
