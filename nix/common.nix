f46a569c049e6c72888a5395a7af31392baf96aa
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-08-25 02:45:15 +0200

    clean

--- nix/common.nix
+++ nix/common.nix
@@ -20,249 +20,249 @@ in
   ];
   fonts.fontconfig.enable = true;
 
-  home.enableDebugInfo = true;
+  home = {
+    enableDebugInfo = true;
+    stateVersion = "23.05"; # Please read the comment before changing.
+    keyboard = {
+      layout = "fr";
+      variant = "bepo";
+    };
 
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+    packages = with pkgs; [
+      acpi
+      age
+      arandr
+      black
+      brightnessctl
+      broot
+      cachix
+      cargo
+      cargo-binstall
+      cargo-release
+      cava
+      ccze
+      cntr
+      comma
+      dcfldd
+      dconf
+      deadnix
+      dfc
+      d-spy
+      dig
+      dos2unix
+      chrpath
+      clang_17
+      clang-tools
+      cmake
+      cmake-format
+      cntr
+      docker-compose
+      du-dust
+      element-desktop-wayland
+      evince
+      #eww-wayland
+      eza
+      fd
+      ffmpeg
+      file
+      firefox-devedition
+      fish
+      fishPlugins.bass
+      font-awesome
+      gdb
+      gdbgui
+      ghostscript
+      git
+      gnupg
+      gtklock
+      gtklock-userinfo-module
+      gtklock-powerbar-module
+      gtklock-playerctl-module
+      grim
+      helix
+      htop
+      httpie
+      hugo
+      hwloc
+      i3lock
+      iftop
+      inetutils
+      imv
+      iotop
+      i3
+      i3lock
+      i3status-rust
+      isort
+      jless
+      jq
+      just
+      khal
+      khard
+      killall
+      kitty
+      kolourpaint
+      less
+      libreoffice
+      lix
+      lsd
+      lsof
+      gnumake
+      mdcat
+      mdbook
+      meld
+      meshlab
+      mosh
+      mpv
+      mypy
+      ncdu
+      ninja
+      nitrogen
+      nixfmt-rfc-style
+      (nixpkgs-review.override {
+        nix = lix;
+        withNom = true;
+      })
+      #nix-du
+      nix-diff
+      nix-init
+      nix-output-monitor
+      nix-tree
+      nix-update
+      nmap
+      noto-fonts-emoji
+      nurl
+      okular
+      openldap
+      openssh
+      openssl
+      pandoc
+      pavucontrol
+      pdfarranger
+      pdfpc
+      #pipewire
+      pinentry-qt
+      pipx
+      plantuml
+      pkg-config
+      playerctl
+      (poetry.withPlugins (ps: with ps; [ poetry-plugin-up ]))
+      (python3.withPackages (
+        ps: with ps; [
+          django
+          httpx
+          i3ipc
+          ipython
+          jinja2
+          ldap3
+          numpy
+          pandas
+          pandocfilters
+          pip
+          pyarrow
+          pyyaml
+          tabulate
+          tqdm
+          wand
+          wheel
+        ]
+      ))
+      pre-commit
+      pre-commit-sort
+      pulseaudio
+      pavucontrol
+      pwgen
+      ripgrep
+      rofi-emoji
+      rofi-file-browser
+      rofi-power-menu
+      rofi-rbw
+      rpi-imager
+      ruff
+      rustc
+      nur.repos.nim65s.sauce-code-pro
+      signal-desktop
+      sd
+      sccache
+      shellcheck
+      slurp
+      snapcast
+      sops
+      source-han-mono
+      source-han-sans
+      source-han-serif
+      source-sans
+      source-serif
+      spotify
+      sqlite
+      ssh-to-age
+      statix
+      strace
+      swappy
+      #sway
+      swaylock
+      tig
+      tinc
+      thunderbird
+      todoman
+      tree
+      unzip
+      usbutils
+      ventoy
+      vdirsyncer
+      vlc
+      vscode-fhs
+      watchexec
+      wev
+      #wireplumber
+      wl-clipboard
+      xorg.xkill
+      waybar
+      wget
+      wtype
+      xclip
+      wdisplays
+      xdotool
+      xdg-desktop-portal
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-wlr
+      xwayland
+      yt-dlp
+      zathura
+      zeal
+      zellij
+      zola
+      zoom-us
+    ];
 
-  home.keyboard = {
-    layout = "fr";
-    variant = "bepo";
-  };
+    pointerCursor = {
+      package = pkgs.gruppled-white-lite-cursors;
+      name = "gruppled_white_lite";
+      gtk.enable = true;
+    };
 
-  home.packages = with pkgs; [
-    acpi
-    age
-    arandr
-    black
-    brightnessctl
-    broot
-    cachix
-    cargo
-    cargo-binstall
-    cargo-release
-    cava
-    ccze
-    cntr
-    comma
-    dcfldd
-    dconf
-    deadnix
-    dfc
-    d-spy
-    dig
-    dos2unix
-    chrpath
-    clang_17
-    clang-tools
-    cmake
-    cmake-format
-    cntr
-    docker-compose
-    du-dust
-    element-desktop-wayland
-    evince
-    #eww-wayland
-    eza
-    fd
-    ffmpeg
-    file
-    firefox-devedition
-    fish
-    fishPlugins.bass
-    font-awesome
-    gdb
-    gdbgui
-    ghostscript
-    git
-    gnupg
-    gtklock
-    gtklock-userinfo-module
-    gtklock-powerbar-module
-    gtklock-playerctl-module
-    grim
-    helix
-    htop
-    httpie
-    hugo
-    hwloc
-    i3lock
-    iftop
-    inetutils
-    imv
-    iotop
-    i3
-    i3lock
-    i3status-rust
-    isort
-    jless
-    jq
-    just
-    khal
-    khard
-    killall
-    kitty
-    kolourpaint
-    less
-    libreoffice
-    lix
-    lsd
-    lsof
-    gnumake
-    mdcat
-    mdbook
-    meld
-    meshlab
-    mosh
-    mpv
-    mypy
-    ncdu
-    ninja
-    nitrogen
-    nixfmt-rfc-style
-    (nixpkgs-review.override {
-      nix = lix;
-      withNom = true;
-    })
-    #nix-du
-    nix-diff
-    nix-init
-    nix-output-monitor
-    nix-tree
-    nix-update
-    nmap
-    noto-fonts-emoji
-    nurl
-    okular
-    openldap
-    openssh
-    openssl
-    pandoc
-    pavucontrol
-    pdfarranger
-    pdfpc
-    #pipewire
-    pinentry-qt
-    pipx
-    plantuml
-    pkg-config
-    playerctl
-    (poetry.withPlugins (ps: with ps; [ poetry-plugin-up ]))
-    (python3.withPackages (
-      ps: with ps; [
-        django
-        httpx
-        i3ipc
-        ipython
-        jinja2
-        ldap3
-        numpy
-        pandas
-        pandocfilters
-        pip
-        pyarrow
-        pyyaml
-        tabulate
-        tqdm
-        wand
-        wheel
-      ]
-    ))
-    pre-commit
-    pre-commit-sort
-    pulseaudio
-    pavucontrol
-    pwgen
-    ripgrep
-    rofi-emoji
-    rofi-file-browser
-    rofi-power-menu
-    rofi-rbw
-    rpi-imager
-    ruff
-    rustc
-    nur.repos.nim65s.sauce-code-pro
-    signal-desktop
-    sd
-    sccache
-    shellcheck
-    slurp
-    snapcast
-    sops
-    source-han-mono
-    source-han-sans
-    source-han-serif
-    source-sans
-    source-serif
-    spotify
-    sqlite
-    ssh-to-age
-    statix
-    strace
-    swappy
-    #sway
-    swaylock
-    tig
-    tinc
-    thunderbird
-    todoman
-    tree
-    unzip
-    usbutils
-    ventoy
-    vdirsyncer
-    vlc
-    vscode-fhs
-    watchexec
-    wev
-    #wireplumber
-    wl-clipboard
-    xorg.xkill
-    waybar
-    wget
-    wtype
-    xclip
-    wdisplays
-    xdotool
-    xdg-desktop-portal
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-wlr
-    xwayland
-    yt-dlp
-    zathura
-    zeal
-    zellij
-    zola
-    zoom-us
-  ];
+    file = {
+      ".config/dfc/dfcrc".source = ../.config/dfc/dfcrc;
+      ".config/kitty/open-actions.conf".source = ../.config/kitty/open-actions.conf;
+      ".config/khal/config".source = ../.config/khal/config;
+      ".config/khard/khard.conf".source = ../.config/khard/khard.conf;
+      ".config/python_keyring/keyringrc.cfg".source = ../.config/python_keyring/keyringrc.cfg;
+      ".config/rofi-rbw.rc".source = ../.config/rofi-rbw.rc;
+      ".config/vdirsyncer/config".source = ../.config/vdirsyncer/config;
+      ".latexmkrc".source = ../.latexmkrc;
+      ".pypirc".source = ../.pypirc;
 
-  home.pointerCursor = {
-    package = pkgs.gruppled-white-lite-cursors;
-    name = "gruppled_white_lite";
-    gtk.enable = true;
-  };
+      ".xinitrc".text = "exec ${lib.getExe pkgs.i3} > ~/.x.log 2> ~/.x.err";
+    };
 
-  home.file = {
-    ".config/dfc/dfcrc".source = ../.config/dfc/dfcrc;
-    ".config/kitty/open-actions.conf".source = ../.config/kitty/open-actions.conf;
-    ".config/khal/config".source = ../.config/khal/config;
-    ".config/khard/khard.conf".source = ../.config/khard/khard.conf;
-    ".config/python_keyring/keyringrc.cfg".source = ../.config/python_keyring/keyringrc.cfg;
-    ".config/rofi-rbw.rc".source = ../.config/rofi-rbw.rc;
-    ".config/vdirsyncer/config".source = ../.config/vdirsyncer/config;
-    ".latexmkrc".source = ../.latexmkrc;
-    ".pypirc".source = ../.pypirc;
-
-    ".xinitrc".text = "exec ${lib.getExe pkgs.i3} > ~/.x.log 2> ~/.x.err";
-  };
-
-  home.sessionVariables = {
-    SHELL = lib.getExe pkgs.fish;
-    SSH_ASKPASS = "$HOME/scripts/ask_rbw.py";
-    SSH_ASKPASS_REQUIRE = "prefer";
-    SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/ssh-agent";
-    PAGER = "vim -c PAGER -";
-    DELTA_PAGER = "less -FR";
-    MANPAGER = "vim -c ASMANPAGER -";
+    sessionVariables = {
+      SHELL = lib.getExe pkgs.fish;
+      SSH_ASKPASS = "$HOME/scripts/ask_rbw.py";
+      SSH_ASKPASS_REQUIRE = "prefer";
+      SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/ssh-agent";
+      PAGER = "vim -c PAGER -";
+      DELTA_PAGER = "less -FR";
+      MANPAGER = "vim -c ASMANPAGER -";
+    };
   };
 
   accounts.email = {
@@ -352,45 +352,47 @@ in
     theme.name = "Breeze-Dark";
   };
 
-  services.dunst = {
-    enable = true;
-    settings = {
-      global = {
-        follow = "keyboard";
-        geometry = "0x5-10+10";
-        indicate_hidden = true;
-        padding = 10;
-        horizontal_padding = 10;
-        sort = true;
-        format = ''
-          <b>%s</b>
-          %b'';
-        font = "Source Sans";
-        browser = "${lib.getExe pkgs.firefox-devedition} -new-tab";
+  services = {
+    dunst = {
+      enable = true;
+      settings = {
+        global = {
+          follow = "keyboard";
+          geometry = "0x5-10+10";
+          indicate_hidden = true;
+          padding = 10;
+          horizontal_padding = 10;
+          sort = true;
+          format = ''
+            <b>%s</b>
+            %b'';
+          font = "Source Sans";
+          browser = "${lib.getExe pkgs.firefox-devedition} -new-tab";
+        };
       };
     };
-  };
 
-  services.signaturepdf = {
-    enable = true;
-    port = 5165;
-    extraConfig = {
-      max_file_uploads = "201";
-      post_max_size = "24M";
-      upload_max_filesize = "24M";
+    signaturepdf = {
+      enable = true;
+      port = 5165;
+      extraConfig = {
+        max_file_uploads = "201";
+        post_max_size = "24M";
+        upload_max_filesize = "24M";
+      };
     };
-  };
-  services.ssh-agent.enable = true;
+    ssh-agent.enable = true;
 
-  services.spotifyd = {
-    enable = true;
-    settings = {
-      global = {
-        username = "nim65s";
-        password_cmd = "rbw get spotify";
-        device_name = "home-manager";
-        device_type = "computer";
-        backend = "pulseaudio";
+    spotifyd = {
+      enable = true;
+      settings = {
+        global = {
+          username = "nim65s";
+          password_cmd = "rbw get spotify";
+          device_name = "home-manager";
+          device_type = "computer";
+          backend = "pulseaudio";
+        };
       };
     };
   };
