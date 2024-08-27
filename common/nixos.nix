cd7181f655c8ae9f37386d22ba3fbb296c31c280
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-08-27 13:39:06 +0200

    nim65s.cachix.org

--- common/nixos.nix
+++ common/nixos.nix
@@ -25,16 +25,16 @@
     nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
     package = pkgs.lix;
     settings = {
-      substituters = [
-        "https://cache.nixos.org/"
+      extra-substituters = [
         "https://gepetto.cachix.org"
+        "https://nim65s.cachix.org"
         "https://nix-community.cachix.org"
         "https://rycee.cachix.org"
       ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      extra-trusted-public-keys = [
         "gepetto.cachix.org-1:toswMl31VewC0jGkN6+gOelO2Yom0SOHzPwJMY2XiDY="
+        "nim65s.cachix.org-1:aycmWbuJijDcr9npRLM/2X76kY86iToBI2tlkpp2BLE="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "rycee.cachix.org-1:TiiXyeSk0iRlzlys4c7HiXLkP3idRf20oQ/roEUAh/A="
       ];
       experimental-features = [
