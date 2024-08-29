42fa0c5fca5d690a2bdd6b503b96e939e18cb148
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-08-29 17:21:54 +0200

    fish path: gate that

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -1,21 +1,23 @@
-for bin in /bin \
-           /usr/bin \
-           /usr/local/bin \
-           /run/current-system/sw/bin \
-           /nix/var/nix/profiles/default/bin \
-           /etc/profiles/per-user/nim/bin \
-           /opt/openrobots/bin \
-           ~/.cargo/bin \
-           ~/.local/bin \
-           /run/wrappers/bin
-    if test -d $bin
-        if echo $PATH | grep -vq $bin
-            set -pgx PATH $bin
+if test ! -n "$FISH_NO_PATH"
+    for bin in /bin \
+               /usr/bin \
+               /usr/local/bin \
+               /run/current-system/sw/bin \
+               /nix/var/nix/profiles/default/bin \
+               /etc/profiles/per-user/nim/bin \
+               /opt/openrobots/bin \
+               ~/.cargo/bin \
+               ~/.local/bin \
+               /run/wrappers/bin
+        if test -d $bin
+            if echo $PATH | grep -vq $bin
+                set -pgx PATH $bin
+            end
         end
     end
-end
 
-set bin ~/.nix-profile/bin
-if test -d $bin
-    set -pgx PATH $bin
+    set bin ~/.nix-profile/bin
+    if test -d $bin
+        set -pgx PATH $bin
+    end
 end
