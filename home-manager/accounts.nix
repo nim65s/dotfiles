6aadef761b4e8c037ebc63d60b3edce0074a71c1
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-11-11 10:32:23 +0100

    format

--- home-manager/accounts.nix
+++ home-manager/accounts.nix
@@ -12,34 +12,38 @@ in
     email = {
       maildirBasePath = ".mails";
       accounts = {
-        laas = {
-          address = atjoin { name = "guilhem.saurel"; };
-          aliases = [
-            (atjoin { name = "gsaurel"; })
-            (atjoin { name = "saurel"; })
-          ];
-          imap.host = "imap.laas.fr";
-          imap.port = 993;
-          msmtp.enable = true;
-          neomutt.enable = false;
-          notmuch.enable = true;
-          notmuch.neomutt.enable = true;
-          offlineimap.enable = true;
-          passwordCommand = "rbw get --folder laas main";
-          primary = true;
-          realName = "Guilhem Saurel";
-          smtp.host = "smtp.laas.fr";
-          smtp.tls.useStartTls = true;
-          thunderbird = {
-            enable = true;
-            profiles = [ "nim" ];
-            perIdentitySettings = id: {
-              "mail.identity.id_${id}.fcc_reply_follows_parent" = true;
-              "layers.acceleration.disabled" = true; # TODO
+        laas =
+          let
+            mail = atjoin { name = "guilhem.saurel"; };
+          in
+          {
+            address = mail;
+            aliases = [
+              (atjoin { name = "gsaurel"; })
+              (atjoin { name = "saurel"; })
+            ];
+            imap.host = "imap.laas.fr";
+            imap.port = 993;
+            msmtp.enable = true;
+            neomutt.enable = false;
+            notmuch.enable = true;
+            notmuch.neomutt.enable = true;
+            offlineimap.enable = true;
+            passwordCommand = "rbw get --folder laas main";
+            primary = true;
+            realName = "Guilhem Saurel";
+            smtp.host = "smtp.laas.fr";
+            smtp.tls.useStartTls = true;
+            thunderbird = {
+              enable = true;
+              profiles = [ "nim" ];
+              perIdentitySettings = id: {
+                "mail.identity.id_${id}.fcc_reply_follows_parent" = true;
+                "layers.acceleration.disabled" = true; # TODO
+              };
             };
+            userName = "gsaurel";
           };
-          userName = "gsaurel";
-        };
         perso =
           let
             mail = atjoin {
