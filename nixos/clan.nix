b3fd75792c327197ad73862711ae5465f62d7427
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-12-17 23:01:08 +0100

    lacroix

--- nixos/clan.nix
+++ nixos/clan.nix
@@ -15,6 +15,7 @@
       human.ssid = "WNHC-Invite";
       kekeno.ssid = "Kekeno";
       kessica.ssid = "Livebox-CD00";
+      lacroix.ssid = "Freebox-866B60";
       lavelanet.ssid = "Freebox-34A964";
       sabliere.ssid = "Livebox-7730";
       toffan.ssid = "Shannon-WiFi";
