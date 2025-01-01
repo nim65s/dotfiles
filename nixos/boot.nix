1166f429c6d691078f8fb1df6f4f50bb061bd7da
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-02 00:29:52 +0100

    latest zfs kernel

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,5 +1,31 @@
-_: {
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+
+let
+  isUnstable = config.boot.zfs.package == pkgs.zfsUnstable;
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kernelPackages).success
+    && (
+      (!isUnstable && !kernelPackages.zfs.meta.broken)
+      || (isUnstable && !kernelPackages.zfs_unstable.meta.broken)
+    )
+  ) pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
+{
+  # Note this might jump back and worth as kernel get added or removed.
   boot = {
+    kernelPackages = latestKernelPackage;
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
