8121cc6cbd908da7c49dfac5c0f73d27b4bd73bb
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-04 23:42:35 +0100

    ashitaka: base cfg

--- machines/ashitaka/configuration.nix
+++ machines/ashitaka/configuration.nix
@@ -1,7 +1,16 @@
+{ config, ... }:
 {
   imports = [
 
   ];
-
-  # New machine!
+  clan.core.networking = {
+    zerotier.networkId = builtins.readFile (
+      config.clan.core.clanDir + "/machines/loon/facts/zerotier-network-id"
+    );
+  };
+  console.keyMap = "fr-bepo";
+  #networking.hostId = "46dec12f";
+  disko.devices.disk.main.device = "/dev/disk/by-id/nvme-eui.0025385b4140cf80";
+  services.getty.autologinUser = "nim";
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
