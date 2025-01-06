8e162e6071af224d09db42a1eb4bcb0b4311e694
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-06 22:33:46 +0100

    yupa details

--- machines/yupa/configuration.nix
+++ machines/yupa/configuration.nix
@@ -3,4 +3,7 @@
     ../../modules/disko.nix
     ../../modules/shared.nix
   ];
+
+  clan.core.networking.targetHost = "root@192.168.8.104";
+  disko.devices.disk.main.device = "/dev/disk/by-id/nvme-eui.0025388b11b2bd16";
 }
