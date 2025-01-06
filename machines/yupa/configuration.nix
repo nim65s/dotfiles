c95577c53887a644591d3ea7bb381e4a8e7006e0
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-06 23:44:38 +0100

    one more

--- machines/yupa/configuration.nix
+++ machines/yupa/configuration.nix
@@ -5,6 +5,6 @@
     ../../modules/wifi.nix
   ];
 
-  clan.core.networking.targetHost = "root@192.168.8.104";
+  clan.core.networking.targetHost = "root@192.168.8.106";
   disko.devices.disk.main.device = "/dev/disk/by-id/nvme-eui.0025388b11b2bd16";
 }
