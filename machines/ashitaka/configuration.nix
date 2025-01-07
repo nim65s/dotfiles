1154b280bfbbeee4292d74561336d42e9785e579
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-08 00:44:43 +0100

    swaybgs

--- machines/ashitaka/configuration.nix
+++ machines/ashitaka/configuration.nix
@@ -1,3 +1,4 @@
+{ config, lib, pkgs, ... }:
 {
   imports = [
     ../../modules/disko.nix
@@ -13,7 +14,15 @@
 
   disko.devices.disk.main.device = "/dev/disk/by-id/nvme-eui.0025385b4140cf80";
 
-  home-manager.users.user.nim-home.niri = [ ./niri.kdl ];
+  home-manager.users.user.nim-home = {
+    niri = [ ./niri.kdl ];
+    swaybgs = ''
+      ${lib.getExe pkgs.swaybg} -m fill -o DP-1 -i ${../../bg/ashitaka-1.png} &
+      ${lib.getExe pkgs.swaybg} -m fill -o DP-2 -i ${../../bg/ashitaka-2.jpg} &
+      ${lib.getExe pkgs.swaybg} -m fill -o DP-3 -i ${../../bg/ashitaka-3.jpg} &
+      wait
+    '';
+  };
 
   networking = {
     interfaces.enp3s0 = {
--- modules/nim-home.nix
+++ modules/nim-home.nix
@@ -6,6 +6,10 @@
         type = lib.types.listOf lib.types.path;
         default = [ ];
       };
+      swaybgs = lib.mkOption {
+        type = lib.types.str;
+        default = "${lib.getExe pkgs.swaybg} -m fill -i ${config.stylix.image}";
+      };
     };
   };
 
@@ -17,6 +21,20 @@
 
       file = {
         ".config/niri/config.kdl".source = pkgs.concatText "config.kdl" ([ ./niri.kdl ] ++ config.nim-home.niri);
+      };
+    };
+
+    systemd = {
+      user.services = {
+        swaybgs = {
+          Install.WantedBy = [ "graphical-session.target" ];
+          Service.ExecStart = pkgs.writeShellScript "swaybgs" config.nim-home.swaybgs;
+          Unit = {
+            Description = "Set wallpaper(s)";
+            PartOf = "graphical-session.target";
+            After = "graphical-session.target";
+          };
+        };
       };
     };
   };
