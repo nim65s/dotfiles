b7f67fa0ea2e295c98a4921b58661aa05d81038f
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-23 00:42:38 +0100

    perseverance

--- machines/perseverance/configuration.nix
+++ machines/perseverance/configuration.nix
@@ -1,7 +1,14 @@
+{ pkgs, ... }:
 {
   imports = [
-
+    ../../modules/disko.nix
+    ../../modules/nim.nix
+    ../../modules/shared.nix
+    ../../modules/stylix.nix
+    ../../modules/wifi.nix
   ];
 
-  # New machine!
+  clan.core.networking.targetHost = "root@192.168.8.145";
+  disko.devices.disk.main.device = "/dev/disk/by-id/nvme-LDLC_F8+M.2_120_09292220C0589";
+  stylix.image = ../../bg/perseverance.jpeg;
 }
