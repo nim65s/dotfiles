074b1b8689d306120f4116ed51887f921e855c7a
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-01-31 22:21:18 +0100

    clean

--- modules/shared.nix
+++ modules/shared.nix
@@ -94,10 +94,6 @@
         "rycee.cachix.org-1:TiiXyeSk0iRlzlys4c7HiXLkP3idRf20oQ/roEUAh/A="
         "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
       ];
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
     };
   };
 
