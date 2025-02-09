9c261c9d0e4773038524670b1be76dd7c4172cbf
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-02-09 18:12:05 +0100

    yupa ip

--- machines/yupa/configuration.nix
+++ machines/yupa/configuration.nix
@@ -8,7 +8,7 @@
     ../../modules/x86_64-linux.nix
   ];
 
-  clan.core.networking.targetHost = "root@192.168.1.185";
+  clan.core.networking.targetHost = "root@192.168.8.104";
   disko.devices.disk.main.device = "/dev/disk/by-id/nvme-eui.0025388b11b2bd16";
   home-manager.users.nim = import ../../modules/nim-home.nix;
   networking.interfaces."tinc.mars".ipv4.addresses = [
