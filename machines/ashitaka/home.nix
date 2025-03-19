634923079ce70b112b0901308655b8209be6de90
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-03-19 10:47:27 +0100

    DP -> HDMI :(

--- machines/ashitaka/home.nix
+++ machines/ashitaka/home.nix
@@ -19,7 +19,7 @@
     swaybgs = ''
       ${lib.getExe pkgs.swaybg} -m fill -o DP-1 -i ${../../bg/ashitaka-1.png} &
       ${lib.getExe pkgs.swaybg} -m fill -o DP-2 -i ${../../bg/ashitaka-2.jpg} &
-      ${lib.getExe pkgs.swaybg} -m fill -o DP-3 -i ${../../bg/ashitaka-3.jpg} &
+      ${lib.getExe pkgs.swaybg} -m fill -o HDMI-A-1 -i ${../../bg/ashitaka-3.jpg} &
       wait
     '';
   };
