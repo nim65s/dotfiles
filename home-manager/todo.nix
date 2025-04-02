78a752b09d38a358c8907352296faa36c52bc208
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-04-02 03:39:55 +0200

    auto autocorrect

--- home-manager/todo.nix
+++ home-manager/todo.nix
@@ -304,7 +304,7 @@ in
           # pruneTags = true;
         };
         help = {
-          autocorrect = "prompt";
+          autocorrect = 5;
         };
         hub = {
           protocol = "ssh";
