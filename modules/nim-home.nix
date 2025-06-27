d1a13891dc81f9fb750d502145f51347d60da0bc
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-06-27 14:16:45 +0200

    kitty: configure nvim scrollback

--- modules/nim-home.nix
+++ modules/nim-home.nix
@@ -90,7 +90,12 @@ in
 
       kitty = {
         enable = true;
+        actionAliases = {
+          "kitty_scrollback_nvim" =
+            "kitten ${pkgs.vimPlugins.kitty-scrollback-nvim}/python/kitty_scrollback_nvim.py";
+        };
         keybindings = {
+          "kitty_mod+a" = "kitty_scrollback_nvim --config ksb_builtin_last_cmd_output";
           "kitty_mod+u" = "launch --location=vsplit --cwd=current";
           "kitty_mod+k" = "launch --location=hsplit --cwd=current";
           "kitty_mod+r" = "previous_window";
@@ -98,9 +103,11 @@ in
           "kitty_mod+," = "new_tab_with_cwd";
           "kitty_mod+h" = "previous_tab";
           "kitty_mod+g" = "next_tab";
-          "kitty_mod+p" = "show_scrollback";
+          # "kitty_mod+p" = "show_scrollback";
+          "kitty_mod+p" = "kitty_scrollback_nvim";
         };
         settings = {
+          allow_remote_control = "socket-only";
           background_opacity = config.stylix.opacity.terminal;
           cursor_trail = "1";
           enable_audio_bell = false;
@@ -108,10 +115,12 @@ in
           focus_follows_mouse = true;
           font_family = config.stylix.fonts.monospace.name;
           font_size = config.stylix.fonts.sizes.terminal;
+          listen_on = "unix:\${HOME}/.kitty-remote";
           scrollback_pager_history_size = 2;
           shell = "fish";
           tab_bar_style = "powerline";
         };
+        shellIntegration.mode = "enabled";
       };
 
       msmtp.enable = true;
