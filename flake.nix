7c4138b70b53dd832f6a5d29fabb7e08773ab259
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-08-09 14:07:25 +0200

    nix flake update

--- flake.nix
+++ flake.nix
@@ -8,12 +8,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     clan-core = {
-      url = "https://git.clan.lol/nim65s/clan-core/archive/multi-wifi.tar.gz";
+      url = "https://git.clan.lol/clan/clan-core/archive/main.tar.gz";
       inputs = {
         flake-parts.follows = "flake-parts";
-        # flake-utils.follows = "flake-utils";
         nixpkgs.follows = "nixpkgs";
-        # nuschtos.follows = "nuschtosSearch";
         treefmt-nix.follows = "treefmt-nix";
       };
     };
