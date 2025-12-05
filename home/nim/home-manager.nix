20cb15cead7ea513a01b16df6d233ceb946ac5f3
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-12-05 11:20:04 +0100

    hm: auto update/expire

--- home/nim/home-manager.nix
+++ home/nim/home-manager.nix
@@ -1,4 +1,8 @@
+# Standalone HM things
+# ie. not nixos hm
 {
+  config,
+  lib,
   pkgs,
   stylix,
   ...
@@ -11,6 +15,22 @@
 
   home = {
     packages = [ pkgs.nix ];
+  };
+  services.home-manager = {
+    autoExpire = {
+      enable = true;
+      frequency = "weekly";
+      store.cleanup = true;
+      store.options = "--delete-older-than 30d";
+    };
+    autoUpgrade = {
+      enable = true;
+      flakeDir = "${config.nim-home.homeDirectory}/dotfiles";
+      useFlake = true;
+    };
+  };
+  systemd.user.services.home-manager-auto-upgrade = {
+    Service.ExecStartPre = "${lib.getExe pkgs.git} pull https://github.com/nim65s/dotfiles";
   };
   xdg.portal = {
     enable = true;
