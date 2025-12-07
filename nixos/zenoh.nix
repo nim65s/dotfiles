864d6bded78d9df412c93090aa41aceb012a0832
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-12-08 00:50:48 +0100

    zenoh: storee azv

--- nixos/zenoh.nix
+++ nixos/zenoh.nix
@@ -6,7 +6,13 @@
   services.zenohd = {
     enable = true;
     plugins = [ pkgs.zenoh-plugin-mqtt ];
-    settings.plugins.mqtt = { };
+    settings.plugins = {
+      mqtt = { };
+      storage_manager.storages.azv = {
+        key_expr = "azv/**";
+        volume.id = "memory";
+      };
+    };
   };
   networking.firewall.allowedTCPPorts = [ 7447 ];
 }
