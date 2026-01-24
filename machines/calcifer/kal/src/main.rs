5bbd916c3c6b44547b6649161f8f2fb6894357fb
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-01-24 12:00:52 +0100

    kal: blink

--- machines/calcifer/kal/src/main.rs
+++ machines/calcifer/kal/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use std::{thread, time};
+
+use anyhow::Result;
+use gpio_cdev::{Chip, LineRequestFlags};
+
+fn main() -> Result<()> {
+    let mut chip = Chip::new("/dev/gpiochip0")?;
+    let output = chip
+        .get_line(17)?
+        .request(LineRequestFlags::OUTPUT, 0, "kal")?;
+
     println!("Hello, world!");
+
+    loop {
+        output.set_value(1)?;
+        thread::sleep(time::Duration::from_secs(1));
+        output.set_value(0)?;
+        thread::sleep(time::Duration::from_secs(1));
+    }
 }
