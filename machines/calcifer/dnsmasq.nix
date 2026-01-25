67d0b01f5c8304f339405c7b93cccdf0ab189bb9
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-01-25 17:58:33 +0100

    calcifer: more dnsmasq

--- machines/calcifer/dnsmasq.nix
+++ machines/calcifer/dnsmasq.nix
@@ -20,12 +20,19 @@
       except-interface = "wan";
       bind-interfaces = true;
 
-      # IPv4 DHCP ranges
+      address = [
+        "/calcifer.azv/192.168.1.1"
+        "/calcifer.azv/2a01:e0a:989:7191::1"
+        "/calcifer.azv.saurel.me/82.65.234.111"
+        "/calcifer.azv.saurel.me/2a01:e0a:989:7190::1"
+      ];
       dhcp-range = [
         "lan1,192.168.1.100,192.168.1.200,12h"
         "lan1,::,constructor:lan1,ra-only"
         # "lan2,192.168.2.100,192.168.2.200,12h"
+        # "lan2,::,constructor:lan2,ra-only"
         # "lan3,192.168.3.100,192.168.3.200,12h"
+        # "lan2,::,constructor:lan3,ra-only"
       ];
 
       enable-ra = true;
@@ -35,6 +42,7 @@
       domain = config.networking.domain;
       local = "/${config.networking.domain}/";
       dhcp-authoritative = true;
+      expand-hosts = true;
     };
   };
 
