ed70b36b773ad9e3a06b963c528f4464cd93c1ea
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-01-25 22:10:35 +0100

    kal: logs

--- machines/calcifer/kal/src/main.rs
+++ machines/calcifer/kal/src/main.rs
@@ -7,14 +7,17 @@ use kal::Daemon;
 async fn main() -> Result<()> {
     env_logger::init();
 
+    log::info!("Init...");
+
     let mut chip = Chip::new("/dev/gpiochip0")?;
+    log::debug!("chip: {chip:?}");
     let relay = chip
         .get_line(17)?
         .request(LineRequestFlags::OUTPUT, 0, "kal")?;
-
-    println!("Hello, world!");
+    log::debug!("relay: {relay:?}");
 
     let mut daemon = Daemon::init(relay).await;
+    log::info!("Start !");
     loop {
         daemon.select().await;
     }
