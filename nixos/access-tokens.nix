58b41b30a1e6ba550053b1d3ef7b1e854333a449
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-02-13 11:54:16 +0100

    give access token to nim

--- nixos/access-tokens.nix
+++ nixos/access-tokens.nix
@@ -3,6 +3,7 @@
   clan.core.vars.generators.access-tokens = {
     prompts.tokens-input.description = "your nix access-tokens, eg. 'github.com=ghp_eHWJ…xq0'";
     files.conf.secret = true;
+    files.conf.owner = "nim";
     script = ''
       echo "access-tokens = $(cat $prompts/tokens-input)" > $out/conf
     '';
@@ -10,6 +11,10 @@
   };
 
   nix.extraOptions = ''
+    !include ${config.clan.core.vars.generators.access-tokens.files.conf.path}
+  '';
+
+  home-manager.users.nim.nix.extraOptions = ''
     !include ${config.clan.core.vars.generators.access-tokens.files.conf.path}
   '';
 }
