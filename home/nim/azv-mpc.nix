d18c9ba39a78fee5d8ef4f85bf94eaf047858d12
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-08 20:50:02 +0100

    mpc

--- home/nim/azv-mpc.nix
+++ home/nim/azv-mpc.nix
@@ -5,7 +5,7 @@
 }:
 {
   systemd.user.services.azv-mpc = {
-    Service.ExecStart = "${lib.getExe' pkgs.snapcast "snapclient"} --player pipewire tcp://spare.azv";
+    Service.ExecStart = "${lib.getExe' pkgs.snapcast "snapclient"} --player pipewire tcp://spare.w";
     Unit.Description = "Listen to calcifer mopidy/snapserver";
   };
 }
