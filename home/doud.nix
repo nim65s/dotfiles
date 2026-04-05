409351f06ce058eb71807de0de22e2f56771b886
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-05 16:06:49 +0200

    solarium

---
+++ home/doud.nix
@@ -0,0 +1,70 @@
+{
+  catppuccin,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    catppuccin.homeModules.catppuccin
+  ];
+
+  config = {
+    catppuccin = {
+      enable = true;
+      accent = "sapphire";
+    };
+
+    home = {
+      homeDirectory = "/home/doud";
+      username = "doud";
+      keyboard.layout = "fr";
+      packages = [
+        pkgs.vlc
+      ];
+      stateVersion = "25.05";
+    };
+
+    programs = {
+      element-desktop.enable = true;
+      fish.enable = true;
+      firefox = {
+        enable = true;
+        languagePacks = [
+          "fr"
+          "en"
+        ];
+      };
+      kitty = {
+        enable = true;
+        settings = {
+          enabled_layouts = "splits,fat,tall,grid,horizontal,vertical,stack";
+          focus_follows_mouse = true;
+          scrollback_pager_history_size = 2;
+          shell = "fish";
+          tab_bar_style = "powerline";
+        };
+      };
+      starship.enable = true;
+    };
+
+    qt = {
+      enable = true;
+      platformTheme.name = "kvantum";
+      style.name = "kvantum";
+    };
+
+    stylix = {
+      cursor = {
+        name = "catppuccin-mocha-sapphire-cursors";
+        package = pkgs.catppuccin-cursors.mochaSapphire;
+        size = 24;
+      };
+      targets = {
+        firefox.enable = false;
+        mako.enable = false; # silence a HM assert
+        qt.enable = false;
+        starship.enable = false;
+      };
+    };
+  };
+}
--- machines/solarium/configuration.nix
+++ machines/solarium/configuration.nix
@@ -1,7 +1,43 @@
 {
+  pkgs,
+  ...
+}:
+{
   imports = [
-
+    ../../nixos/disko/zfs.nix
+    ../../nixos/display.nix
+    ../../nixos/laptop.nix
+    ../../nixos/shared.nix
+    ../../nixos/nixos.nix
+    ../../nixos/systemd-boot.nix
   ];
 
-  # New machine!
+  console = {
+    useXkbConfig = false;
+    keyMap = "fr";
+  };
+  disko.devices.disk.main = {
+    device = "/dev/disk/by-id/ata-WDC_WD5000LPVX-08V0TT5_WD-WXA1A25E1ECV";
+    name = "main-5960c9943c8b84f2d5158243d19fb2fc";
+  };
+  home-manager.users = {
+    nim = import ../../home/nim/main.nix;
+    doud = import ../../home/doud.nix;
+  };
+  programs.waybar.enable = false;
+  services = {
+    displayManager = {
+      autoLogin.user = "doud";
+      defaultSession = null;
+    };
+    desktopManager.plasma6.enable = true;
+    xserver.xkb.variant = "";
+  };
+  stylix.image = ../../bg/hattori.jpg;
+  users.users.doud = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    # group = "kids";
+  };
+
 }
