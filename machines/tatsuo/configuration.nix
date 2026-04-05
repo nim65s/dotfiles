c71fecab775d255e40d4a1714acfd1cd06d288d1
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-06 01:08:47 +0200

    tatsuo

--- machines/tatsuo/configuration.nix
+++ machines/tatsuo/configuration.nix
@@ -1,7 +1,18 @@
 {
   imports = [
-
+    ../../nixos/disko/ext4-swap-tmpfs.nix
+    ../../nixos/display.nix
+    ../../nixos/shared.nix
+    ../../nixos/nixos.nix
+    ../../nixos/systemd-boot.nix
   ];
 
-  # New machine!
+  disko.devices.disk.main = {
+    device = "/dev/disk/by-id/ata-Samsung_SSD_870_EVO_2TB_S754NX0Y712985D";
+    name = "main-5960c9943c8b84243d19fb3f2d5158fc";
+  };
+  home-manager.users = {
+    nim = import ../../home/nim/main.nix;
+  };
+  # stylix.image = ../../bg/hattori.jpg;
 }
