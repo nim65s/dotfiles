de2be800ce38d79abf91eedbbcd69bd4b5d97149
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-29 13:52:00 +0200

    mopidy -> music assistant

--- machines/spare/azv.nix
+++ machines/spare/azv.nix
@@ -3,13 +3,9 @@
   pkgs,
   ...
 }:
-let
-  fifo = "/run/mopidy-snapserver.fifo";
-in
 {
   imports = [
     ../../nixos/small.nix
-    ../../nixos/mopidy.nix
     ../../nixos/nas.nix
   ];
 
@@ -17,6 +13,17 @@ in
     "x-systemd.after=systemd-networkd-wait-online@eno1.service"
     "x-systemd.requires=systemd-networkd-wait-online@eno1.service"
   ];
+
+  networking.firewall.allowedTCPPorts = [
+    8080
+    9090
+    9777
+    1704 # snapcast
+    1780 # snapserver
+    8095 # music-assistant
+  ];
+
+  # kodi
 
   services.displayManager = {
     autoLogin.user = "kodi";
@@ -36,66 +43,40 @@ in
     };
   };
 
-  networking.firewall.allowedTCPPorts = [
-    8080
-    9090
-    9777
-    1704 # snapcast
-    1780 # snapserver
-    6600 # mpd
-    6680 # mpd http
-  ];
+  # music-assistant
 
   services = {
-    mopidy.settings = {
-      audio.output = "audioresample ! audioconvert ! audio/x-raw,rate=48000,channels=2,format=S16LE ! filesink location=${fifo}";
-      mpd.hostname = "::";
-      m3u.playlists_dir = "/var/lib/mopidy/Playlists";
-      http.hostname = "spare.w";
-    };
-
-    nim-mopidy = {
+    music-assistant = {
       enable = true;
-      enable-nixos = true;
-    };
-
-    snapserver = {
-      enable = true;
-      settings = {
-        stream.source = "pipe://${fifo}?name=mopidy";
-        http.enabled = true;
-      };
+      openFirewall = true;
+      providers = [
+        "filesystem_nfs"
+        "listenbrainz_scrobble"
+        "lrclib"
+        "sendspin"
+        "snapcast"
+      ];
     };
   };
+
+  # snapclient
 
   systemd = {
     services = {
-      mopidy.after = [ "systemd-tmpfiles-setup.service" ];
       snapclient = {
         serviceConfig = {
-          User = "snapserver";
-          Group = "snapserver";
+          User = "snapclient";
+          Group = "snapclient";
           ExecStart = "${lib.getExe' pkgs.snapcast "snapclient"} tcp://localhost";
         };
       };
-      snapserver = {
-        serviceConfig = {
-          DynamicUser = lib.mkForce false;
-          User = "snapserver";
-          Group = "snapserver";
-        };
-      };
     };
-    tmpfiles.rules = [
-      "p ${fifo} 0640 mopidy snapserver - -"
-    ];
   };
 
-  users.groups.snapserver = { };
-  users.users.mopidy.extraGroups = [ "snapserver" ];
-  users.users.snapserver = {
+  users.groups.snapclient = { };
+  users.users.snapclient = {
     isSystemUser = true;
-    group = "snapserver";
+    group = "snapclient";
     extraGroups = [ "audio" ];
   };
 }
