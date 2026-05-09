398f7087c15538afb64fc814e0b8f4f5db246c68
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-05-09 18:06:03 +0200

    bump cmeel

--- pkgs/cmeel.nix
+++ pkgs/cmeel.nix
@@ -1,39 +1,44 @@
 {
   lib,
-
-  python3Packages,
+  python3,
   fetchFromGitHub,
-
-  cmake,
-  git-archive-all,
+  installShellFiles,
 }:
 
-python3Packages.buildPythonApplication rec {
+python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "cmeel";
-  version = "0.58.0";
+  version = "0.60.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "cmake-wheel";
     repo = "cmeel";
-    tag = "v${version}";
-    hash = "sha256-wTM4xn/+YKQ++5bageOb+FX9qiDxo8JJNzGlIWIYOAE=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Z5MsFjm5Zng4zIu8BAcF9dhrLk14z17LFhHRZaSuUUg=";
   };
 
+  nativeBuildInputs = [
+    installShellFiles
+    python3.pkgs.argcomplete
+  ];
+
   build-system = [
-    python3Packages.hatchling
+    python3.pkgs.hatchling
   ];
 
-  dependencies = [
-    python3Packages.tomli
+  dependencies = with python3.pkgs; [
+    tomli
   ];
 
-  optional-dependencies = {
+  optional-dependencies = with python3.pkgs; {
     build = [
       cmake
       git-archive-all
-      python3Packages.packaging
-      python3Packages.wheel
+      packaging
+      wheel
+    ];
+    cli = [
+      argcomplete
     ];
   };
 
@@ -41,12 +46,19 @@ python3Packages.buildPythonApplication rec {
     "cmeel"
   ];
 
+  postInstall = ''
+    installShellCompletion --cmd cmeel \
+      --bash <(register-python-argcomplete --shell bash cmeel) \
+      --fish <(register-python-argcomplete --shell fish cmeel) \
+      --zsh <(register-python-argcomplete --shell zsh cmeel)
+  '';
+
   meta = {
     description = "Create Wheel from CMake projects";
     homepage = "https://github.com/cmake-wheel/cmeel";
-    changelog = "https://github.com/cmake-wheel/cmeel/blob/${src.tag}/CHANGELOG.md";
+    changelog = "https://github.com/cmake-wheel/cmeel/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = lib.licenses.bsd2;
     maintainers = with lib.maintainers; [ nim65s ];
     mainProgram = "cmeel";
   };
-}
+})
