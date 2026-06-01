908039872379145201d6308cc1261da372ad5d0d
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-06-02 01:44:14 +0200

    thunderbird profile

--- home/nim/programs/default.nix
+++ home/nim/programs/default.nix
@@ -167,21 +167,33 @@ in
 
     thunderbird = {
       enable = config.nim-home.gui;
-      profiles.nim = {
-        isDefault = true;
-        settings = {
-          "extensions.activeThemeID" = "{f6d05f0c-39a8-5c4d-96dd-4852202a8244}";
-          "mail.identity.default.compose_html" = 1;
-          "mail.pane_config.dynamic" = lib.mkDefault 2;
-          "mail.server.default.check_all_folders_for_new" = true;
-          "mail.uidensity" = 0;
-          "mail.uifontsize" = 10;
-          "ldap_2.autoComplete.useDirectory" = true;
-          "ldap_2.servers.laas.description" = "Serveur LDAP LAAS";
-          "ldap_2.servers.laas.filename" = "ldap.sqlite";
-          "ldap_2.servers.laas.maxHits" = 100;
-          "ldap_2.servers.laas.uri" = "ldap://ldap2.laas.fr/dc=laas,dc=fr%20??sub?(objectClass=person)";
-          "ldap_2.servers.default.attrmap.PrimaryEmail" = "laas-mainMail";
+      profiles = {
+        nim = {
+          isDefault = true;
+          settings = {
+            "extensions.activeThemeID" = "{f6d05f0c-39a8-5c4d-96dd-4852202a8244}";
+            "mail.identity.default.compose_html" = 1;
+            "mail.pane_config.dynamic" = lib.mkDefault 2;
+            "mail.server.default.check_all_folders_for_new" = true;
+            "mail.uidensity" = 0;
+            "mail.uifontsize" = 10;
+            "ldap_2.autoComplete.useDirectory" = true;
+            "ldap_2.servers.laas.description" = "Serveur LDAP LAAS";
+            "ldap_2.servers.laas.filename" = "ldap.sqlite";
+            "ldap_2.servers.laas.maxHits" = 100;
+            "ldap_2.servers.laas.uri" = "ldap://ldap2.laas.fr/dc=laas,dc=fr%20??sub?(objectClass=person)";
+            "ldap_2.servers.default.attrmap.PrimaryEmail" = "laas-mainMail";
+          };
+        };
+        ttnn = {
+          settings = {
+            "extensions.activeThemeID" = "{f6d05f0c-39a8-5c4d-96dd-4852202a8244}";
+            "mail.identity.default.compose_html" = 1;
+            "mail.pane_config.dynamic" = lib.mkDefault 2;
+            "mail.server.default.check_all_folders_for_new" = true;
+            "mail.uidensity" = 0;
+            "mail.uifontsize" = 10;
+          };
         };
       };
     };
