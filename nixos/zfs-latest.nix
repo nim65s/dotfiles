14d79b09c56ef29b893850fb4681f0b28184c340
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-06-07 18:48:21 +0200

    boot.zfs.forceImportRoot

--- nixos/zfs-latest.nix
+++ nixos/zfs-latest.nix
@@ -32,4 +32,7 @@ in
 {
   # Note this might jump back and worth as kernel get added or removed.
   boot.kernelPackages = lib.mkIf (lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.zfs) latestKernelPackage;
+
+  # evaluation warning: `boot.zfs.forceImportRoot` is using the default value of `true`. It is highly recommended to set it to `false`, the new default from 26.11 on, to reduce the risk of data loss. Alternatively, you can silence this warning by explicitly setting it to `true`.
+  boot.zfs.forceImportRoot = false;
 }
