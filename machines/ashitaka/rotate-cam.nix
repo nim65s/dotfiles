818cee59f4a6b884f1975a27e01197dc91549141
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-07-29 13:52:52 +0200

    rotate-cam: obs

--- machines/ashitaka/rotate-cam.nix
+++ machines/ashitaka/rotate-cam.nix
@@ -1,17 +1,9 @@
 {
-  config,
-  pkgs,
   ...
 }:
 {
-  boot.kernelModules = [ "v4l2loopback" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=10 card_label="rotated" exclusive_caps=1
-  '';
-
-  environment.systemPackages = with pkgs; [
-    ffmpeg
-    v4l-utils
-  ];
+  programs.obs-studio = {
+    enable = true;
+    enableVirtualCamera = true;
+  };
 }
