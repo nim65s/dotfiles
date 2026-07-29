8251fc73c6e423324f80bafd094140db9a37023c
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-07-29 14:20:38 +0200

    obs: cuda + plugins

--- machines/ashitaka/rotate-cam.nix
+++ machines/ashitaka/rotate-cam.nix
@@ -1,9 +1,15 @@
 {
+  pkgs,
   ...
 }:
 {
   programs.obs-studio = {
     enable = true;
     enableVirtualCamera = true;
+    package = pkgs.obs-studio.override { cudaSupport = true; };
+    plugins = with pkgs.obs-studio-plugins; [
+      droidcam-obs
+      obs-backgroundremoval
+    ];
   };
 }
