91c6c11ef884f1abc7c7391508c26f8f9656ebd3
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-08-24 13:30:09 +0200

    jrl-cmakemodules-script 2.3.0

--- overlay.nix
+++ overlay.nix
@@ -23,9 +23,17 @@ in
     '';
   });
   # https://github.com/NixOS/nixpkgs/pull/551441
-  jrl-cmakemodules-scripts = prev.jrl-cmakemodules-scripts.overrideAttrs (super: {
-    nativeBuildInputs = super.nativeBuildInputs ++ [ final.git ];
-  });
+  jrl-cmakemodules-scripts = prev.jrl-cmakemodules-scripts.overrideAttrs (
+    finalAttrs: previousAttrs: {
+      version = "2.3.0";
+      src = final.fetchFromGitHub {
+        inherit (previousAttrs.src) owner repo;
+        tag = "v${finalAttrs.version}";
+        hash = "sha256-PjEE/JIb6gegW5fqKiFgN0th8Fi58Pe0u5qrdIz2Rm8=";
+      };
+      nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ final.git ];
+    }
+  );
   # https://github.com/NixOS/nixpkgs/pull/555902
   music-assistant = prev.music-assistant.overrideAttrs (super: {
     disabledTests =
