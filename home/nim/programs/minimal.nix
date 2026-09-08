7274071ab1f880b641fbbe60f4079def54f1a300
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-09-08 11:05:57 +0200

    ~ -> $HOME

--- home/nim/programs/minimal.nix
+++ home/nim/programs/minimal.nix
@@ -10,8 +10,8 @@
   programs.bash = {
     enable = true;
     profileExtra = ''
-      if [ -f "~/.nix-profile/etc/profile.d/nix.sh" ]; then
-        . "~/.nix-profile/etc/profile.d/nix.sh"
+      if [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/nix.sh"
       fi
     '';
   };
